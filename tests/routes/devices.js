93793039978e9331854d367bf211f3f169ef8902
Fabian Gutierrez <fega.hg@gmail.com>
2017-05-02 10:56:00 -0500

    Added some test and Windows testing integration

--- tests/routes/devices.js
+++ tests/routes/devices.js
@@ -1,11 +1,11 @@
 /* eslint-env node, mocha */
 const request = require('supertest')
 const expect = require('chai').expect
-const app = require('../../app.js')
+const app = require('../../src/app')
 
 describe('/devices', () => {
   describe('GET /devices', () => {
-    it('Respond with JSON', (done) => {
+    it('Respond with JSON with devices', (done) => {
       request(app)
       .get('/devices')
       .expect('Content-Type', /json/)
@@ -17,5 +17,25 @@ describe('/devices', () => {
       })
       .expect(200, done)
     })
+  })
+  describe('GET /devices/:device', () => {
+    it('Should get the requested device')
+  })
+  describe('POST /devices', function (done) {
+    it('Should create a new device')
+    it('Should not create a duplicate device')
+    it('Should not create a device without name')
+    it('Should not create a device without password')
+  })
+  describe('PUT /devices/:device', function (done) {
+    it('Should update an existing device')
+    it('Should not create a new device')
+  })
+  describe('DELETE /devices/:device', function (done) {
+    it('Should delete an existing device')
+    it('Should alert if the device isnt exist')
+  })
+  describe(' PATCH  /devices',function(done){
+    it('Should send 501 not implemented')
   })
 })
---
+++ tests/routes/index.js
@@ -0,0 +1,17 @@
+/* eslint-env node, mocha */
+const request = require('supertest')
+const app = require('../../src/app')
+
+describe('/', () => {
+  describe('GET /', () => {
+    it('Respond with an HTML', (done) => {
+      request(app)
+      .get('/')
+      .expect('Content-Type', /html/)
+      .expect('Content-Type', /charset/)
+      //.expect('Server', /Arduair/)
+      .expect(200)
+      .expect(200, done)
+    })
+  })
+})
