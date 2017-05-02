2def075ad9f477769cd067588be27d52ed749c33
Fabian Enrique Gutierrez <fega.hg@gmail.com>
2017-05-01 20:47:11 -0500

    Added first test

--- tests/routes/devices.js
+++ tests/routes/devices.js
@@ -7,8 +7,14 @@ describe('/devices', () => {
   describe('GET /devices', () => {
     it('Respond with JSON', (done) => {
       request(app)
-      .get('/user')
+      .get('/devices')
       .expect('Content-Type', /json/)
+      .expect(200)
+      .expect(res => {
+        expect(res.body).to.have.any.keys({'status': 'success'})
+        expect(res.body).to.have.any.keys('data')
+        expect(res.body.data).to.have.all.keys('devices')
+      })
       .expect(200, done)
     })
   })
